ef35947bb85dfdafd01af6dceb0e1b47d8de3832
satheesh <satheesh@satheesh-PC>
2018-03-30 20:22:05 +0530

    Smita commited successfully.....

---
+++ ffff/src/ffff/hello.java
@@ -0,0 +1,12 @@
+package ffff;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello mccoy");
+		System.out.println("Smita love u");
+
+	}
+
+}
