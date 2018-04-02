50c74e67376ee450ecb0681493cb1c0d81bed764
satheesh <satheesh@satheesh-PC>
2018-04-02 15:23:58 +0530

    commit maven

---
+++ mavenP/src/main/java/com/mccoy/mavenP/App.java
@@ -0,0 +1,14 @@
+package com.mccoy.mavenP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! smita" );
+        
+    }
+}
